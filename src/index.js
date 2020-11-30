0d80654c97f45b62d227645757483e8ed121feab
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2020-11-29 22:22:08 -0800

    index js

---
+++ src/index.js
@@ -0,0 +1,82 @@
+import Game from './game';
+import StartScreen from './start_screen';
+
+
+// grabs the id's and sets variables as written
+document.addEventListener("DOMContentLoaded", () => {
+    const page = document.getElementById("page")
+    const canvas = document.getElementById("game-canvas");
+    const ctx = canvas.getContext("2d");
+    const input = document.getElementById("typing-form")
+ 
+// to create a new startscreen class for startscreen file
+    const startScreen = new StartScreen(ctx, canvas);
+    const game = new Game(canvas, ctx, page, input);
+
+    // game.testThis();
+
+// this is the title drops screen once you load the page
+    //start-screen
+    let titlePosition = 200;
+    let startCounter = 0;
+
+    function titleDrop() {
+        // debugger
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        titlePosition += 6;
+        if (titlePosition >= 350) {
+            titlePosition = 350;
+            startCounter += .5;
+            if (startCounter % 10 <= 6) {
+                startScreen.drawStartClick();
+            } else {
+                null;
+            }
+
+            canvas.addEventListener('click', game.start)
+            page.addEventListener('keydown', game.start)
+        }
+
+        startScreen.drawTitle(titlePosition)
+    }
+
+
+    if (canvas.className === 'start-screen') {
+        input.style.display = 'none';
+        window.startInterval = setInterval(titleDrop, 70)
+    }
+
+
+
+    //Audio
+    function playAudio() {
+        gameAudio.play()
+    }
+    
+    
+    function pauseAudio() {
+        gameAudio.pause()
+    }
+
+
+    let gameAudio = document.getElementById('game-audio')
+    const playBtn = document.getElementById('play-audio')
+    const pauseBtn = document.getElementById('pause-audio')
+
+    playBtn.addEventListener('click', function(e) {
+        e.preventDefault()
+        playAudio()
+        playBtn.setAttribute('class', 'clear-music-button')
+        pauseBtn.removeAttribute('class', 'clear-music-button')
+    })
+
+    pauseBtn.addEventListener('click', function(e) {
+        e.preventDefault()
+        pauseAudio()
+        pauseBtn.setAttribute('class', 'clear-music-button')
+        playBtn.removeAttribute('class', 'clear-music-button')
+    })
+
+});
+
+
